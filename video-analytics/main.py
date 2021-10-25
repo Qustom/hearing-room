bb735242fdcec94bfd5a98192bbb299a01b90f8d
qustom <custom456@gmail.com>
2021-10-25 08:20:15 -0700

    Some optimizations

--- video-analytics/main.py
+++ video-analytics/main.py
@@ -2,8 +2,11 @@ import cv2
 import face_recognition
 import json
 
-SKIP_FRAME_COUNT = 10
-RECOG_RESIZE = 2
+# Some variables to speed up processing
+# Number of frame skips until processing images
+SKIP_FRAME_COUNT = 60 # Higher faster, less data
+# How much times to shrink the image for processing
+RECOG_RESIZE = 1 # Higher faster, less accurate, Minimum of 1.
 
 def process_video(filename: str) -> None:
     mov = cv2.VideoCapture(filename)
@@ -15,9 +18,9 @@ def process_video(filename: str) -> None:
     known_faces = []
     timestamps = []
 
-    # create a frame on GPU for images
+    # create a frame on GPU for resizing images
     gpu_frame = cv2.cuda_GpuMat()
-    skip = SKIP_FRAME_COUNT
+    skip_processing = 0
 
     while True:
         ret_val, frame = mov.read()
@@ -26,42 +29,48 @@ def process_video(filename: str) -> None:
         if not ret_val:
             break
 
-        # send frame to GPU for resize
-        gpu_frame.upload(frame)
-        gpu_small = cv2.cuda.resize(gpu_frame,(0, 0), fx=1/RECOG_RESIZE, fy=1/RECOG_RESIZE)
-        small_frame = gpu_small.download()
+        # Skip some frames
+        if skip_processing == 0:
+            skip_processing = SKIP_FRAME_COUNT
 
-        # Convert to face_recognition format
-        face_frame = small_frame[:, :, ::-1]
+            # send frame to GPU for resize
+            gpu_frame.upload(frame)
+            gpu_small = cv2.cuda.resize(gpu_frame,(0, 0), fx=1/RECOG_RESIZE, fy=1/RECOG_RESIZE)
+            small_frame = gpu_small.download()
 
-        # Get all face positions and encodings in frame
-        face_pos = face_recognition.face_locations(face_frame, model="cnn")
-        face_encodings = face_recognition.face_encodings(face_frame, face_pos)
+            # Convert to face_recognition format
+            face_frame = small_frame[:, :, ::-1]
 
-        # Check each face found
-        for face_encoding, pos in zip(face_encodings, face_pos):
-            matches = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.60)
+            # Get all face positions and encodings in frame
+            face_pos = face_recognition.face_locations(face_frame, model="cnn")
+            face_encodings = face_recognition.face_encodings(face_frame, face_pos)
 
-            # If there are any matches to a previous encoding, then append the timestamp for it
-            if any(matches):
-                for index, match in enumerate(matches):
-                    if match:
-                        timestamps[index].append(mov.get(cv2.CAP_PROP_POS_MSEC))
-            # Otherwise, add tne new encoding to the list, save image to file, and setup timestamping
-            else:
-                known_faces.append(face_encoding)
-                img = frame[pos[0]*RECOG_RESIZE: pos[2]*RECOG_RESIZE, pos[3]*RECOG_RESIZE: pos[1]*RECOG_RESIZE]
-                cv2.imwrite(f"output/face-{len(known_faces)}.png", img)
-                timestamps.append([mov.get(cv2.CAP_PROP_POS_MSEC)])
-            
-        print(mov.get(cv2.CAP_PROP_POS_MSEC))
+            # Check each face found
+            for face_encoding, pos in zip(face_encodings, face_pos):
+                matches = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.60)
+
+                # If there are any matches to a previous encoding, then append the timestamp for it
+                if any(matches):
+                    for index, match in enumerate(matches):
+                        if match:
+                            timestamps[index].append(mov.get(cv2.CAP_PROP_POS_MSEC))
+                # Otherwise, add tne new encoding to the list, save image to file, and setup timestamping
+                else:
+                    known_faces.append(face_encoding)
+                    img = frame[pos[0]*RECOG_RESIZE: pos[2]*RECOG_RESIZE, pos[3]*RECOG_RESIZE: pos[1]*RECOG_RESIZE]
+                    cv2.imwrite(f"output/face-{len(known_faces)}.png", img)
+                    timestamps.append([mov.get(cv2.CAP_PROP_POS_MSEC)])
+        else:
+            skip_processing -= 1
+                
+        print(f"Time (MSEC): {mov.get(cv2.CAP_PROP_POS_MSEC)}")
 
         if cv2.waitKey(1) == 27:
             break 
 
     # Save timestamp data to file
     data = {"timestamps": timestamps}
-    with open(r"output\data.txt", 'w') as outfile:
+    with open(r"output\data.json", 'w') as outfile:
         json.dump(data, outfile)
 
 process_video(r"C:\Users\qustom\source\ai-news-reader\video-analytics\raw_data\pexal.mp4")
